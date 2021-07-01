11630f23a6a5f9cd6be635e85faaa8ef718d4dce
rejan bajracharya <rejanbajracharya@bajratechnologies.com>
2021-07-02 01:40:29 +0545

    usage of ejs as a templating engine

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const shopRoutes = require('./routes/shop');
 const rootDir = require('./utils/path'); 
 
 const app = express();
-app.set('view engine', 'pug'); 
+app.set('view engine', 'ejs'); 
 app.set('views', 'views'); 
 
 app.use(express.json()); 
@@ -16,7 +16,7 @@ app.use('/admin',adminData.router);
 app.use(shopRoutes);
 
 app.use((req,res,next) => {
-    res.status(404).render('404.pug', {docTitle: 'Page not found'});
+    res.status(404).render('404', {docTitle: 'Page not found', path:'/error-page'});
 })
 
 
