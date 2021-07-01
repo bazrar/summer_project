64655288aabf34dbaad436b0d909fa2ca2ffe8fe
rejan bajracharya <rejanbajracharya@bajratechnologies.com>
2021-07-02 04:08:55 +0545

    code refactor for product model

--- models/product.js
+++ models/product.js
@@ -1,36 +1,31 @@
 const fs = require('fs'); 
 const path = require('path'); 
 const rootdir = require('../utils/path'); 
-const {promisify} = require('util');
 
 const p = path.join(rootdir, 'data', 'products.json'); 
+const getProductsFromFile = () => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(p, (err, data) => {
+            if(!err) {
+                resolve(JSON.parse(data))
+            }
+            reject(err);
+        });
+    }); 
+}
 
 class Product { 
     constructor(title) {
         this.title = title; 
     }
 
-    save() {
-        fs.readFile(p, (err, data) => {
-            let products = []; 
-            if(!err) {
-                products = JSON.parse(data); 
-            }
-            products.push(this); 
-            fs.writeFile(p, JSON.stringify(products), err => console.log(err)); 
-
-        });
+    save = async () => {
+        let products = await getProductsFromFile(); 
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => console.log(err));
     }
-
    static fetchAll() {
-       return new Promise((resolve, reject) => {
-           fs.readFile(p, (err, data) => {
-               if(!err) {
-                   resolve(JSON.parse(data))
-               }
-               reject(err);
-           });
-       }); 
+       return getProductsFromFile();
    }
 }
 
